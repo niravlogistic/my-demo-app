8b5dddbedce2033552955f5a768746f3f27dd886
Nirav <niravjadatiya@gmail.com>
2019-09-28 17:13:15 +0530

    Started Using requestAnimationFrame, removed setInterval()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { change, reset } from './store/actions/my.actions';
 import { AppState } from './store/reducers';
-declare var window: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,9 +15,12 @@ export class AppComponent {
   }
 
   onClickStart() {
-    this.timer = setInterval(() => {
-      this.start();
-    }, 1000);
+    // this.timer = setInterval(() => {
+    //   this.start();
+    // }, 1000);
+
+    // without using setInterval
+    this.timer = requestAnimationFrame(this.render);
   }
 
   start() {
@@ -26,10 +28,23 @@ export class AppComponent {
   }
 
   stop() {
-    clearInterval(this.timer);
+    // clearInterval(this.timer);
+
+    // without using setInterval
+    cancelAnimationFrame(this.timer);
   }
 
   reset() {
     this.store.dispatch(reset());
+  }
+
+  render = (now) => {
+    // each 1 seconds call the start() function
+    if (!this.last || now - this.last >= 1 * 1000) {
+      // console.log('last ', this.last, ' now ', now, new Date());
+      this.last = now;
+      this.start();
+    }
+    this.timer = requestAnimationFrame(this.render);
   }
 }
